4489e7794a0d04c5cdaa218093b7f2ccfc434843
mao <qq1044467857@gmail.com>
2019-06-21 17:19:24 +0800

    fixed test1.py, add step to export .pth

--- examples/a_simple_example/test1.py
+++ examples/a_simple_example/test1.py
@@ -59,56 +59,58 @@ def acc_val_classes(net, testloader, classes):
 # model = load_model('./my_model.h5')
 
 
-# model_file_name = './my_model.h5'
-# model = pickle_from_file(model_file_name)
+model_file_name = './my_model.h5'
+model = pickle_from_file(model_file_name)
+torch_model = model.graph.produce_model()
+torch.save(torch_model, 'test.pth')
 # results = model.evaluate(x_test, y_test)
 # print(results)
 
 # plot_model(model, to_file='model.png')
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-net = torch.load('./entire_torch.pth')
-net.to(device)
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-# x_train = x_train / 255.0
-x_train = np.reshape(x_train, (60000, 1, 28, 28)).astype('float32') / 255.0
-x_test = np.reshape(x_test, (10000, 1, 28, 28)).astype('float32') / 255.0
-y_train = np.array(y_train).astype('int64')
-y_test = np.array(y_test).astype('int64')
-
-train = data_utils.TensorDataset(torch.from_numpy(x_train), torch.from_numpy(y_train))
-trainloader = data_utils.DataLoader(train, batch_size=50, shuffle=True)
-
-test = data_utils.TensorDataset(torch.from_numpy(x_test), torch.from_numpy(y_test))
-testloader = data_utils.DataLoader(test, batch_size=1, shuffle=False)
-
-classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.5)
-
-for epoch in range(2000):
-    running_loss = 0.0
-    exp_lr_scheduler.step()
-
-    for i, data in enumerate(trainloader):
-        inputs, labels = data[0].to(device), data[1].to(device)
-
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-        if i == 0:
-            print("epoch {}, train_loss: {:.5f}".format(epoch, running_loss))
-    if epoch % 10 == 0:
-        # acc_val(net, testloader)
-        acc_val_classes(net, testloader, classes)
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#
+# net = torch.load('./entire_torch.pth')
+# net.to(device)
+#
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# # x_train = x_train / 255.0
+# x_train = np.reshape(x_train, (60000, 1, 28, 28)).astype('float32') / 255.0
+# x_test = np.reshape(x_test, (10000, 1, 28, 28)).astype('float32') / 255.0
+# y_train = np.array(y_train).astype('int64')
+# y_test = np.array(y_test).astype('int64')
+#
+# train = data_utils.TensorDataset(torch.from_numpy(x_train), torch.from_numpy(y_train))
+# trainloader = data_utils.DataLoader(train, batch_size=50, shuffle=True)
+#
+# test = data_utils.TensorDataset(torch.from_numpy(x_test), torch.from_numpy(y_test))
+# testloader = data_utils.DataLoader(test, batch_size=1, shuffle=False)
+#
+# classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# criterion = nn.CrossEntropyLoss()
+# optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+# exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.5)
+#
+# for epoch in range(2000):
+#     running_loss = 0.0
+#     exp_lr_scheduler.step()
+#
+#     for i, data in enumerate(trainloader):
+#         inputs, labels = data[0].to(device), data[1].to(device)
+#
+#         optimizer.zero_grad()
+#         outputs = net(inputs)
+#         loss = criterion(outputs, labels)
+#         loss.backward()
+#         optimizer.step()
+#
+#         running_loss += loss.item()
+#         if i == 0:
+#             print("epoch {}, train_loss: {:.5f}".format(epoch, running_loss))
+#     if epoch % 10 == 0:
+#         # acc_val(net, testloader)
+#         acc_val_classes(net, testloader, classes)
 #
 # input = np.reshape(x_train[1], (1,1,28,28)).astype('float32')
 #
